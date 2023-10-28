56024667538d6f07b7dc11daf6b7abceed60815a
Adeousn Samuel Oluwatomisin <ibsam26700749@gmail.com>
2023-10-28 19:15:02 +0100

    i  <Navbar/>

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Aboutme from '../../Component/Aboutme'
+import Footer from '../../Component/Footer'
+import Hello from '../../Component/Hello'
+import Navbar from '../../Component/Navbar'
+import Worksandproject from '../../Component/Worksandproject'
+import './App.css'
+
+function App() {
+
+
+  return (
+   <>
+   <Navbar/>
+   <br />
+   <Hello/>
+   <Aboutme/>
+   <Worksandproject/>
+   <Footer/>
+   </>
+  )
+}
+
+export default App
